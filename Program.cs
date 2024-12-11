416962f02d462ea7741f218fc9dfa006b5a3de03
Runos <Runos@RUNOSTY>
2024-12-11 20:53:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Приклад для double
+        double a = 5.5, b = 10.2;
+        Console.WriteLine($"Before Swap: a = {a}, b = {b}");
+        Swap(ref a, ref b);
+        Console.WriteLine($"After Swap: a = {a}, b = {b}\n");
+
+        // Приклад для string
+        string str1 = "Hello", str2 = "World";
+        Console.WriteLine($"Before Swap: str1 = {str1}, str2 = {str2}");
+        Swap(ref str1, ref str2);
+        Console.WriteLine($"After Swap: str1 = {str1}, str2 = {str2}");
+    }
+
+    // Універсальний метод для обміну значеннями
+    static void Swap<T>(ref T x, ref T y)
+    {
+        T temp = x;
+        x = y;
+        y = temp;
+    }
+}
